5a2e5063fd2ff4f8b20eba50b9c9e928137f3972
dbenbass <dbenbass@gmail.com>
2018-11-26 13:16:41 -0500

    Made changes to models and serializers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 2018_11_26_180401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,19 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "schemes", force: :cascade do |t|
+    t.string "name"
+    t.date "date"
+    t.string "idea"
+    t.string "category"
+    t.string "feasibility"
+    t.boolean "highlight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_schemes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +47,5 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "schemes", "users"
 end
