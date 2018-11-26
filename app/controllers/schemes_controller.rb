b8199f413baa2f1e76f02d36db9b9ae6f3f4b9a0
dbenbass <dbenbass@gmail.com>
2018-11-26 15:52:04 -0500

    Changed schemes controller

---
+++ app/controllers/schemes_controller.rb
@@ -0,0 +1,53 @@
+class SchemesController < OpenReadController
+  before_action :set_scheme, only: %i[show update destroy]
+
+  # GET /schemes
+  def index
+    @schemes = Scheme.all
+
+    render json: @schemes
+  end
+
+  # GET /schemes/1
+  def show
+    render json: @scheme
+  end
+
+  # POST /schemes
+  def create
+    @scheme = current_user.schemes.build(scheme_params)
+    # require 'pry'
+    # binding.pry
+
+    if @scheme.save
+      render json: @scheme, status: :created, location: @scheme
+    else
+      render json: @scheme.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /schemes/1
+  def update
+    if @scheme.update(scheme_params)
+      render json: @scheme
+    else
+      render json: @scheme.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /schemes/1
+  def destroy
+    @scheme.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_scheme
+      @scheme = Scheme.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def scheme_params
+      params.require(:scheme).permit(:name, :date, :idea, :category, :feasibility, :highlight)
+    end
+end
